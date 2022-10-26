04fcf845b9c4dcf99bc826c1417ad37c55c4baf4
Tran Xuan Bach <trxbach135@gmail.com>
2022-10-26 20:22:45 +0700

    what is gitignore

--- header/functions.h
+++ header/functions.h
@@ -11,6 +11,7 @@ namespace functions{
     template <typename F, size_t argc = 2>
     class function{
     public:
+        template <typename F>
         function(const F& lamb): func(lamb){
             
         }
